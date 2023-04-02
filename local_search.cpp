d6f4f1b8cbaeafd20b5eda310a0cc0c2e0428a3f
IgorCzudy <igor.czudy@student.put.poznan.pl>
2023-04-02 16:44:35 +0200

    feat, fix: locla search, seepset, cycle_length fix

--- local_search.cpp
+++ local_search.cpp
@@ -1,10 +1,13 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <math.h>
 #include <list>
 #include <map>
+#include <random>
+#include <utility>
 
-std::list<int> edge_replacment(std::vector<int> path, int random_start, int random_to){ // random statment are not lots to easy testing
+std::vector<int> edge_replacment(std::vector<int> path, int random_start, int random_to){ // random statment are not lots to easy testing
     // std::list path = std::list(path.begin(), path.end());
     std::list<int> path_with_replec_edge;
 
@@ -18,7 +21,7 @@ std::list<int> edge_replacment(std::vector<int> path, int random_start, int rand
     std::list<int> third_slice = std::list<int>(path.begin()+random_to+1, path.end());
     path_with_replec_edge.insert(path_with_replec_edge.end(), third_slice.begin(), third_slice.end());
 
-    return path_with_replec_edge;
+    return std::vector(path_with_replec_edge.begin(), path_with_replec_edge.end());
 }
 
 
@@ -31,7 +34,8 @@ int cycle_length(std::vector<std::vector<int>> distance_matrix, std::list<int> c
         }
         prev = *it;
     }
-    return length;
+    auto it = cycle.begin();
+    return length += distance_matrix[prev][*it];
 }
 
 int count_delta_function(std::vector<std::vector<int>> distance_matrix, std::vector<int> old_path, std::vector<int> new_path){
@@ -39,4 +43,46 @@ int count_delta_function(std::vector<std::vector<int>> distance_matrix, std::vec
    std::list new_path_l = std::list(new_path.begin(), new_path.end());
 
    return cycle_length(distance_matrix, new_path_l) -  cycle_length(distance_matrix, old_path_l);
+}
+
+
+std::vector<int> greedy_local_search(std::vector<int> staring_path, std::vector<std::vector<int>> distance_matrix){
+
+    std::vector<int> best_path = staring_path;
+
+    while (true){
+        int random_start = rand() % staring_path.size();
+        int random_to = rand()%(staring_path.size()-random_start + 1) + random_start;
+        std::vector<int> propose_path = edge_replacment(best_path, random_start-1, random_to-1);
+        if (count_delta_function(distance_matrix, best_path, propose_path) < 0){
+            return propose_path;
+        }
+    }
+}
+
+std::pair<std::vector<int>, std::vector<int>> greedy_local_search_for_pair(std::pair<std::list<int>, std::list<int>> staring_paths, std::vector<std::vector<int>> distance_matrix){
+
+    return std::pair(greedy_local_search(std::vector(staring_paths.first.begin(), staring_paths.first.end()), distance_matrix), greedy_local_search(std::vector(staring_paths.second.begin(), staring_paths.second.end()), distance_matrix));
+
+}
+
+
+std::vector<int>steepest_local_search(std::vector<int> staring_path, std::vector<std::vector<int>> distance_matrix){
+
+    std::vector<int> best_path = staring_path;
+    int i = 500;
+    while (i>0){
+        i--;
+        std::vector<int> propose_path = greedy_local_search(staring_path, distance_matrix);
+        if (count_delta_function(distance_matrix, best_path, propose_path) < 0){
+            best_path = propose_path;
+        }
+    }
+    return best_path;
+}
+
+
+std::pair<std::vector<int>, std::vector<int>> steepest_local_search_for_pair(std::pair<std::list<int>, std::list<int>> staring_paths, std::vector<std::vector<int>> distance_matrix){
+    return std::pair(steepest_local_search(std::vector(staring_paths.first.begin(), staring_paths.first.end()), distance_matrix), steepest_local_search(std::vector(staring_paths.second.begin(), staring_paths.second.end()), distance_matrix));
+
 }
--- main.cpp
+++ main.cpp
@@ -11,40 +11,37 @@
 
 int main(){
 
-    std::vector<Node> nodes_corrdinats = read_data("kroA100.tsp");
-    
-    std::vector<std::vector<int>> distance_matrix = create_distance_matrix(nodes_corrdinats);
-    
-    std::pair cycles = create_two_tsp_cycles_regret_heurystic(distance_matrix, 0);
-
-    // std::cout<<"length1 : "<<cycle_length(distance_matrix, c.first) <<" length2 : "<<cycle_length(distance_matrix, c.second)<<"\n"; 
-
-    // for (auto i: cycles.first){
-    //     std::cout << i << " ";
-    // }  
-    // std::cout << std::endl; 
-
-    // wymiana krawędzi testy 
-    std::list<int> replec_edge =  edge_replacment({1,2,3,4,5,6,7,8,9}, 1, 5); // random statment are not lots to easy testing
-    std::list<int> correct = {1,2,6,5,4,3,7,8,9};
+    // wymiana krawędzi testy     
+    std::vector<int> replec_edge =  edge_replacment({1,2,3,4,5,6,7,8,9}, 1, 5); // random statment are not lots to easy testing
+    std::vector<int> correct = {1,2,6,5,4,3,7,8,9};
     assert(std::equal(replec_edge.begin(), replec_edge.end(), correct.begin()));
 
-    std::list<int> replec_edge_2 =  edge_replacment({1,2,3,4,5,6,7,8,9}, 0, 2); // random statment are not lots to easy testing
-    std::list<int> correct_2 = {1,3,2,4,5,6,7,8,9};
+    std::vector<int> replec_edge_2 =  edge_replacment({1,2,3,4,5,6,7,8,9}, 0, 2); // random statment are not lots to easy testing
+    std::vector<int> correct_2 = {1,3,2,4,5,6,7,8,9};
     assert(std::equal(replec_edge_2.begin(), replec_edge_2.end(), correct_2.begin()));
 
-    std::list<int> replec_edge_3 =  edge_replacment({1,2,3,4,5,6,7,8,9}, 6, 8); // random statment are not lots to easy testing
-    std::list<int> correct_3 = {1,2,3,4,5,6,7,9,8};
+    std::vector<int> replec_edge_3 =  edge_replacment({1,2,3,4,5,6,7,8,9}, 6, 8); // random statment are not lots to easy testing
+    std::vector<int> correct_3 = {1,2,3,4,5,6,7,9,8};
     assert(std::equal(replec_edge_3.begin(), replec_edge_3.end(), correct_3.begin()));
 
     //count_delta_function testy 
-    // liczy dla ścieżki nie dla cyklu, co jest błędne 
-
     // minimalizujemy
     //dla lepszej starej zwróci l.dodatnią
     // a dla lepszej nowej ujemną 
     int wynik =  count_delta_function({{0,3,1,6},{3,0,5,2},{1,5,0,4},{6,2,4,0}}, {0,1,2,3}, {0,3,2,1});
-    std::cout << wynik << std::endl;
+    assert(wynik == 0);
+
+
+    std::vector<Node> nodes_corrdinats = read_data("kroA100.tsp");
+    std::vector<std::vector<int>> distance_matrix = create_distance_matrix(nodes_corrdinats);
+    std::pair cycles = create_two_tsp_cycles_regret_heurystic(distance_matrix, 0);
+    std::cout<<"regret heuristic: length1 : "<<cycle_length(distance_matrix, cycles.first) <<" length2 : "<<cycle_length(distance_matrix, cycles.second)<<"\n"; 
+    
+    std::pair<std::vector<int>, std::vector<int>> greedy_paths = greedy_local_search_for_pair(cycles, distance_matrix);
+    std::cout<<"greedy local search: length1 : "<<cycle_length(distance_matrix, std::list(greedy_paths.first.begin(), greedy_paths.first.end())) <<" length2 : "<<cycle_length(distance_matrix, std::list(greedy_paths.second.begin(), greedy_paths.second.end()))<<"\n"; 
+
+    std::pair<std::vector<int>, std::vector<int>> steepeset_paths = steepest_local_search_for_pair(cycles, distance_matrix);
+    std::cout<<"steeepsetlength1 : "<<cycle_length(distance_matrix, std::list(steepeset_paths.first.begin(), steepeset_paths.first.end())) <<" length2 : "<<cycle_length(distance_matrix, std::list(steepeset_paths.second.begin(), steepeset_paths.second.end()))<<"\n"; 
 
     return 0;
 }
